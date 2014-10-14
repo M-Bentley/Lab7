fad729e491b479bafd6d1e4b95b8827428dba904
M-Bentley <s530942@student.roundrockisd.org>
2014-10-14 13:33:46 -0500

    Second commit

---
+++ lab7.py
@@ -0,0 +1,23 @@
+num = 1
+while (num < 300):
+    print num
+    num = num + 2
+    
+list1 = [0,1,5,84,684,3,19,11,20,'ball']
+step = 0
+
+while (step < len(list1)):
+    print list1[step]
+    step = step +1
+    
+import random
+rand = random.randint(0,50)
+
+while (userInput < or > rand):
+    print 'Guess a number between 0 & 50'
+    userInput = raw_input()
+    if userInput == rand:
+        print 'Correct'
+    elif print 'Try Again'
+    
+    
